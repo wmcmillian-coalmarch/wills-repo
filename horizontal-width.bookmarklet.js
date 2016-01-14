522cf318ce476425daef6a310db78084a2ea6aa0
Will <littlmac0@gmail.com>
2016-01-14 11:28:50 -0500

    added pdeploy

---
+++ horizontal-width.bookmarklet.js
@@ -0,0 +1,14 @@
+javascript:(function(){
+    var css = '* { border: 1px solid #f00; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+})();
