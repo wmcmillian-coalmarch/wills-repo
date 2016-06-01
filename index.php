bacc06268209a864af0bb9f1d441fe264cc2295b
Will <littlmac0@gmail.com>
2016-06-01 16:50:07 -0400

    updated scripts to use DRUPAL_ROOT instead of pwd

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
+?>
 
-$date = new DateTime('20160202T182453Z');
-$date->setTimezone(new DateTimeZone('UTC'));
-
-print $date->format('Y-m-d H:i:s e');
+<iframe src="http://docs.google.com/gview?url=https%3A%2F%2Fwww.dropbox.com%2Fs%2F4veqj45z4du3jrv%2FBarr_DUKE%25209.24.15.pdf%3Fdl%3D1&embedded=true&chrome=false&a=bi" style="position:absolute; width: 100%; height: 100%; top:0; right:0; bottom:0; left: 0;" frameborder="0"></iframe>
