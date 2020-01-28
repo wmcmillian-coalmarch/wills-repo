9852bcb9373da41d95f54c06e891a53c5aa02a02
Will McMillian <wmcmillian@coalmarch.com>
2020-01-28 15:44:46 -0500

    created call scripts update script

---
+++ scripts/updateCallScriptsSite.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+source ${HOME}/bin/getSiteEnv.sh;
+
+terminus backup:create $SITE.$ENV;
+
+if [ ! -d ~/Sites/$SITE ]; then
+    siteget $SITE $ENV;
+else
+    prefresh $SITE $ENV;
+fi
+
+cd ~/Sites/$SITE;
+
+
+if [ ! $ENV = 'dev' ]; then
+    terminus backup:create $SITE.dev;
+fi
+
+terminus upstream:updates:apply $SITE.dev --updatedb
+
+drush cex -y;
+needsPush=0;
+if [[ `git status --porcelain` ]]; then
+    needsPush=1;
+    git add .
+    git commit -m "Config export"
+fi
+
+git pull --no-edit
+
+drush updatedb -y;
+
+if [ $needsPush = "1" ]; then
+    git push;
+fi
+
+pdrush cim --partial -y;
+
+if [ $ENV = 'test' ] || [ $ENV = 'live' ]; then
+    terminus env:deploy "$SITE.test" --note="Upstream Updates" --cc --updatedb;
+    pdrush test cim --partial -y;
+    pdrush test cr
+fi
+
+if [ $ENV = 'live' ]; then
+    terminus env:deploy "$SITE.live" --note="Upstream Updates" --cc --updatedb;
+    pdrush live cim --partial -y;
+    pdrush live cr
+fi
