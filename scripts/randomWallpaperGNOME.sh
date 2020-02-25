fcf883bbdebe41914e528a6be566accc739197f9
Will McMillian <wmcmillian@gmail.com>
2020-02-25 16:42:51 -0500

    change wallpaper in cron Gnome

--- scripts/randomWallpaperGNOME.sh
+++ scripts/randomWallpaperGNOME.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env  bash
 set -e
+user=$(id --real --user);
+PID=$(pgrep -u $user gnome-session)
+PID=$(echo $PID | cut -d" " -f1)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-);
+
 DIR=/home/willmcmillian/Pictures/Wallpapers;
 WP=$(find $DIR/ -type f | shuf -n 1);
 
