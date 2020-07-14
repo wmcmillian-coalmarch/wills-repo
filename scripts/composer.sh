3945d50ddd53b9bb3913f529f717258240ccbaa0
Will <littlmac0@gmail.com>
2020-07-14 10:08:42 -0400

    tweaks

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+re="*/php";
+if [[ "$0" =~ $re ]]; then
+    php=$0;
+else
+    php=$(which php);
+fi;
+
 moduleDir=$(php-config --extension-dir)
 options=$(find $moduleDir -maxdepth 1 -name "*.so"| \
 
@@ -13,4 +20,4 @@ options=$(find $moduleDir -maxdepth 1 -name "*.so"| \
     tr --delete '\n'
 )
 # build the final command line
-php --no-php-ini $options -d memory_limit=-1 -d extension=pdo_mysql -d extension=pdo_pgsql ~/bin/composer.phar "${@}"
+$php --no-php-ini $options -d memory_limit=-1 -d extension=pdo_mysql -d extension=pdo_pgsql ~/bin/composer.phar "${@}"
