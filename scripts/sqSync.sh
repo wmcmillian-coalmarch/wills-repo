00d180371f794b7dd0038644dc488d159be1b727
Will <littlmac0@gmail.com>
2020-12-02 15:32:02 -0500

    tweaks

--- scripts/sqSync.sh
+++ scripts/sqSync.sh
@@ -7,3 +7,5 @@ source ${HOME}/bin/getSiteEnv.sh;
 sqSyncCode.sh $SITE $ENV;
 sqSyncDB.sh $SITE $ENV;
 sqSyncFiles.sh $SITE $ENV;
+
+cd ~/${PROJECTDIR}/${SITE};
--- scripts/sqSyncCode.sh
+++ scripts/sqSyncCode.sh
@@ -6,6 +6,8 @@ source ${HOME}/bin/getSiteEnv.sh;
 
 SITEDIR=~/$PROJECTDIR/$SITE;
 
+echo "Syncing code";
+
 if [ ! -d $SITEDIR ]; then
   git clone git@github.com:coalmarch-development/${SITE}.git
 else
